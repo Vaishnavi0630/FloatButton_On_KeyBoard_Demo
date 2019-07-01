a628dab9946506be1a522f2f29ca965cadbcf396
vaishnavi sasane <vaishnavisasane3@gmail.com>
2019-07-01 17:01:46 +0530

    second commit

--- FloatbuttonDemo/UIViewExtension.swift
+++ FloatbuttonDemo/UIViewExtension.swift
@@ -7,3 +7,70 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    private struct AssociatedKeys {
+        static  var CustomView = "nsh_CustomView"
+    }
+    
+    var customView: UIView? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.CustomView) as? UIView
+        }
+        
+        set {
+            if let newValue = newValue {
+                objc_setAssociatedObject(
+                    self,
+                    &AssociatedKeys.CustomView,
+                    newValue as UIView?,
+                    .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+                )
+            }
+        }
+    }
+    
+    private struct AssociatedButtonKeys {
+        static var ContinueBarBtn = "nsh_ContinueBarBtn"
+    }
+    
+    var continueBarBtn: UIButton? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedButtonKeys.ContinueBarBtn) as? UIButton
+        }
+        
+        set {
+            if let newValue = newValue {
+                objc_setAssociatedObject(
+                    self,
+                    &AssociatedButtonKeys.ContinueBarBtn,
+                    newValue as UIButton?,
+                    .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+                )
+            }
+        }
+    }
+    
+    
+    
+    func addInputAccessoryView(textField:UITextField, title: String, target: Any, selector: Selector) {
+        
+        customView =  UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 50))
+        continueBarBtn =  UIButton.init(frame:  CGRect(x: 0, y: 0, width: (customView?.frame.size.width)!, height: 50))
+        customView?.backgroundColor = UIColor.white
+        continueBarBtn?.setTitle(title, for: .normal)
+        continueBarBtn?.titleLabel?.font = UIFont.init(name: "System", size: 18.0)
+        continueBarBtn?.backgroundColor = UIColor.blue
+        continueBarBtn?.isEnabled = true
+        continueBarBtn?.alpha = 0.5
+        continueBarBtn?.addTarget(target, action: selector, for: .touchUpInside)
+        customView?.addSubview(continueBarBtn!)
+        textField.inputAccessoryView = customView
+    }
+    
+}
+
+
+
--- FloatbuttonDemo/ViewController.swift
+++ FloatbuttonDemo/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtsample: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.addInputAccessoryView(textField: txtsample, title: "FLOAT BUTTON", target: self, selector: #selector(clickOnButton))
     }
 
 
+    @objc func clickOnButton(){
+    
+        print("click on float button")
+        
+    }
+    
 }
 
